9179500af7f3d459846abe53c04797ce6d08a754
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2021-12-28 22:48:22 -0300

    Criacao da classe ImportedProduct

---
+++ ImprimirEtiquetas/Entities/ImportedProduct.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace ImprimirEtiquetas.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct()
+        {
+        }
+
+        public ImportedProduct(string name, double price, double customsFee) : base(name, price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public double TotalPrice()
+        {
+            return Price + CustomsFee;
+        }
+
+        public override string PriceTag()
+        {
+            return $"{base.PriceTag()} (Customs fee: $ {CustomsFee.ToString("F2", CultureInfo.InvariantCulture)})";
+        }
+    }
+}
