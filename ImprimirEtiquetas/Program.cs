2fc83cd89a4a3f80753c654de76827963b470e19
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2021-12-28 22:08:14 -0300

    Projeto inciado

---
+++ ImprimirEtiquetas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ImprimirEtiquetas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
