8a10835462692bb9b158269003f34082f29160e3
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2021-12-28 22:26:50 -0300

    Criacao da classe Product

---
+++ ImprimirEtiquetas/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace ImprimirEtiquetas.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+        }
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string priceTag()
+        {
+            return $"{Name} $ {Price.ToString("F2", CultureInfo.InvariantCulture)} ";
+        }
+    }
+}
--- ImprimirEtiquetas/Program.cs
+++ ImprimirEtiquetas/Program.cs
@@ -6,7 +6,7 @@ namespace ImprimirEtiquetas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
